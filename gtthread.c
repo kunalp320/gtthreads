6eda775651822061367f96f1472e7dd737b7295f
Kunal Patel <kunalp2014@gmail.com>
2014-01-22 21:04:28 -0500

    added thread array to keep track of threads added thread_id as a instance of each thread

--- gtthread.c
+++ gtthread.c
@@ -1,11 +1,14 @@
 long quantum;
 
-
+#define MAX_SIZE 20
+#define STACKSIZE 1024
 
 typedef struct {
 	unsigned long int gtthread_id;
 
-}
+}thread_info;
+
+thread_info threads[MAX_SIZE];
 
 void gtthread_init(long period) {
 	quantum = period;

540faac5832acb1e8ee5e29808aa07fc16b1961b
Kunal Patel <kunalp2014@gmail.com>
2014-01-22 21:15:48 -0500

    at init each thread has a thread_id equal to the index of the array

--- gtthread.c
+++ gtthread.c
@@ -3,6 +3,7 @@ long quantum;
 #define MAX_SIZE 20
 #define STACKSIZE 1024
 
+static int i = 0; /* For loop counter */
 typedef struct {
 	unsigned long int gtthread_id;
 
@@ -12,5 +13,11 @@ thread_info threads[MAX_SIZE];
 
 void gtthread_init(long period) {
 	quantum = period;
+	
+	for(i = 0; i<MAX_SIZE; i++) {
+		threads[i].gtthread_id = i;
+	}
+
+
 	
 }

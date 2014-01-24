437eb2618acad2b0187d4a6154c2939ef45df7de
Kunal Patel <kunalp2014@gmail.com>
2014-01-24 10:52:07 -0500

    finished equals method

--- gtthread.c
+++ gtthread.c
@@ -1,37 +1,41 @@
 #include <gtthread.h>
 
-#define MAX_SIZE 20
-#define STACKSIZE 1024
+#define MAX_THREAD_SIZE 20 /* Number of threads that can co-exist */
+#define STACK_SIZE 1024 
 
-long quantum;
-
-static int i = 0; /* For loop counter */
 typedef struct {
 	gtthread_t gtthread_id;
-	unsigned long int number_times_run;
+	void *return_value;
+
 
 }thread_info;
 
-thread_info threads[MAX_SIZE];
+thread_info threads[MAX_THREAD_SIZE];
+static int i = 0; /*For loop counter */
+static long quantum;
+
+static int next_thread;
+static int current_thread;
 
 void gtthread_init(long period) {
 	quantum = period;
 	
-	for(i = 0; i<MAX_SIZE; i++) {
+	for(i = 0; i<MAX_THREAD_SIZE; i++) {
 		threads[i].gtthread_id = i;
-		threads[i].number_times_run = 0;
 	}
-	
-	
-
-	
 	
 }
 int gtthread_equals(gtthread_t t1, gtthread_t t2) {
-	if(t1 == t2) {
-		return 1;
-	}
-	else {
-		return 0;
-	}
+	return t1==t2 ? 1:0;
+}
+void gtthread_exit(void *ret_val) {
+	threads[current].return_value = ret_val;
+	gtthread_cancel(current_thread);
+	/* setcontext() */
+}
+int gtthread_cancel(gtthread_t thread_id) {
+	
+	free(threads[thread_id]);
+	return 1;
+
 }
